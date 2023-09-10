c71ea34c394ce2f89717850a70b1d4e768f7b1f0
Rodrigol <rodrigolb01@gmail.com>
2023-09-10 16:24:21 -0300

    delete users exception handling

--- src/main/java/com/example/demo/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/demo/resources/exceptions/ResourceExceptionHandler.java
@@ -1,14 +1,12 @@
 package com.example.demo.resources.exceptions;
 
 import java.time.Instant;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
+import com.example.demo.services.exceptions.DatabaseException;
 import com.example.demo.services.exceptions.ResourceNotFoundException;
-
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
@@ -20,6 +18,16 @@ public class ResourceExceptionHandler {
 	{
 		String error = "resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		
+		StandardError standardError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(standardError);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request)
+	{
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		
 		StandardError standardError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(standardError);
--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entities.User;
 import com.example.demo.repositories.UserRepository;
+import com.example.demo.services.exceptions.DatabaseException;
 import com.example.demo.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,9 +34,20 @@ public class UserService {
 		return userRepository.save(obj);
 	}
 	
-	public void delete(long id)
+	public void delete(Long id)
 	{
-		userRepository.deleteById(id);
+		try
+		{
+			userRepository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e)
+		{
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e)
+		{
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(long id, User obj)
